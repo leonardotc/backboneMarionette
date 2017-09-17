c0a869e99b8ec681b663aefb9fcd3593c5287d77
Leonardo Cardoso <leonardo.t.cardoso@hotmail.com>
2017-09-17 11:56:39 -0300

    fix code style

--- src/router/controller.js
+++ src/router/controller.js
@@ -7,10 +7,12 @@ const Controller = Object.extend({
   showRoot() {
     console.log("test it")
   },
+
   showVideo(id) {
     const video = new Video({id: '2LtGZJvbjTQ'})
     video.fetch().then((content) => console.log(video))
   },
+
   showVideos(channelId) {
     const videos = new Videos({id: 'UCWRbcEYuQc1Jcl3Wyu6f9sw'})
     videos.fetch().then((content) => console.log("videos", videos))
